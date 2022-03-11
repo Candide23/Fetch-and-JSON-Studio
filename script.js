a39cc63e1cfd5d48d60c8f5d465a682b5970896a
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-03-10 20:15:27 -0600

    made some changes in javascript file

--- script.js
+++ script.js
@@ -5,17 +5,12 @@ window.addEventListener("load", function () {
   ).then(function (response) {
     response.json().then(function (json) {
       const container = document.getElementById("container");
-      let name;
 
       for (let i = 0; i < json.length; i++) {
-        for (let j = 0; j < json[i].skills.length; j++) {
-          json[i].skills[j] = " " + json[i].skills[j];
-        }
-        name = `${json[i].firstName} ${json[i].lastName}`;
-        container.innerHTML += `
+       container.innerHTML += `
                     <div class="astronaut">
                     <div class="bio">
-                        <h3>${name}</h3>
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
                         <ul>
                             <li>Hours in space: ${json[i].hoursInSpace}</li>
                             <li>Active: ${json[i].active}</li>
